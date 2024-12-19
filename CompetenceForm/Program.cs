c47cdd721fb1e2df52b2323b0930a7f72b6ec539
r4faello <rapolas.jonas.labutis@gmail.com>
2024-12-19 10:40:41 +0200

    CORS change

--- CompetenceForm/Program.cs
+++ CompetenceForm/Program.cs
@@ -50,7 +50,7 @@ namespace CompetenceForm
                 options.AddPolicy("AllowSpecificOrigin",
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:3000")
+                        policy.WithOrigins("https://competenceform.netlify.app")
                               .AllowAnyHeader()
                               .AllowAnyMethod();
                     });
