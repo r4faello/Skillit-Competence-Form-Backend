a54c395cd9fe690401623925cedee4b2b60442fc
Rapolas Jonas Labutis <rapolas.jonas.labutis@gmail.com>
2024-12-23 16:33:49 +0200

    Implementation of missing handler

---
+++ CompetenceForm/Handlers/SaveAnsweredQuestionCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CompetenceForm.Commands;
+using CompetenceForm.Common;
+using CompetenceForm.Services.CompetenceService;
+using MediatR;
+
+namespace CompetenceForm.Handlers
+{
+    public class SaveAnsweredQuestionCommandHandler : IRequestHandler<SaveAnsweredQuestionCommand, ServiceResult>
+    {
+        private readonly ICompetenceService _competenceService;
+
+        public SaveAnsweredQuestionCommandHandler(ICompetenceService competenceService)
+        {
+            _competenceService = competenceService;
+        }
+
+        public async Task<ServiceResult> Handle(SaveAnsweredQuestionCommand request, CancellationToken cancellationToken)
+        {
+            return await _competenceService.SaveAnsweredQuestionAsync(
+                request.User,
+                request.CompetenceSetId,
+                request.CompetenceId,
+                request.AnswerId);
+        }
+    }
+
+}
---
+++ CompetenceForm/Migrations/20241223095931_AdjustedModels.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompetenceForm.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drafts_Users_AuthorId",
+                table: "Drafts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubmittedRecords_Users_AuthorId",
+                table: "SubmittedRecords");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "SubmittedRecords",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Drafts",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Drafts_Users_AuthorId",
+                table: "Drafts",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubmittedRecords_Users_AuthorId",
+                table: "SubmittedRecords",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drafts_Users_AuthorId",
+                table: "Drafts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubmittedRecords_Users_AuthorId",
+                table: "SubmittedRecords");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "SubmittedRecords",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Drafts",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Drafts_Users_AuthorId",
+                table: "Drafts",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubmittedRecords_Users_AuthorId",
+                table: "SubmittedRecords",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
